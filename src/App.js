b606810de8a66e98652d1f145d0f88a82f0f7396
JamesAlvine <jamesalvine340@gmail.com>
2022-11-03 15:24:26 +0300

    patahostel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router,Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import ForgotPassword from "./pages/ForgotPassword";
 import Home from "./pages/Home";
