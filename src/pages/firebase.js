e5f93cdce30894aa93b12ec466ea092bb8a6dab2
JamesAlvine <jamesalvine340@gmail.com>
2022-11-21 10:57:22 +0300

    patahostel

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -5,6 +5,14 @@ import {getFirestore} from "firebase/firestore"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey:process.env.REACT_APP_APIKEY ,
+  authDomain:process.env.REACT_APP_AUTHDOMAIN ,
+  projectId:process.env.REACT_APP_PROJECTID ,
+  storageBucket:process.env.REACT_APP_STORAGEBUCKET ,
+  messagingSenderId:process.env.REACT_APP_MESSAGINGSENDERID ,
+  appId:process.env.REACT_APP_APPID 
+};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
