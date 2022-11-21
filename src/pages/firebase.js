c6f532a0f581b60c1e69078dfcf05bef29161bab
JamesAlvine <jamesalvine340@gmail.com>
2022-11-21 10:16:57 +0300

    patahostel

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -5,15 +5,8 @@ import {getFirestore} from "firebase/firestore"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCGwj4i0NcsQybcNO04C4NQxxy8ZixuJS8",
-  authDomain: "pata-hostel-b2eee.firebaseapp.com",
-  projectId: "pata-hostel-b2eee",
-  storageBucket: "pata-hostel-b2eee.appspot.com",
-  messagingSenderId: "640571221755",
-  appId: "1:640571221755:web:b9c01c84e9a5c855d0325e"
-};
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
 export const db = getFirestore()
+
